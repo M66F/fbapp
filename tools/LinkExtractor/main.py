fe8c2d71569f6b301b6a89b150a2858072a58181
PeterUlb <ulbrich-p@web.de>
2015-09-25 23:06:26 +0200

    add player link extraction tool

---
+++ tools/LinkExtractor/main.py
@@ -0,0 +1,63 @@
+
+__author__ = 'Peter'
+
+import requests
+import re
+import codecs
+
+# some magic to remove dupilcates, copy pasterino
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if not (x in seen or seen_add(x))]
+# magic end
+
+
+def main():
+    url = input("Enter team url: ")
+    #url = "http://www.transfermarkt.de/fc-bayern-munchen/startseite/verein/27"
+    if not ((bool(re.search('transfermarkt\.de', url))) or (bool(re.search('verein', url)))):
+        print("Invalid URL!")
+        return
+
+    # Extract links from website
+    # regex to search, format and then put into a file
+    try:
+        ##############################################
+        s = requests.Session()
+        s.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36'
+        r = s.get(url)
+        content = r.text
+        ##############################################
+    except:
+        print("Request to server (" + url + ")" + "failed [Code 1]")
+        return
+
+    m = re.findall("href=\"\/.{0,20}\/profil\/spieler\/\d*", content)
+    print(m)
+
+    links = [ ]
+    for link in m:
+        links.append(re.findall('\/.*', link))
+
+
+    linkList = []
+    for link in links:
+        linkList.append('http://www.transfermarkt.de' + (str(link).replace('[','').replace(']','').replace('\'','') ))
+
+
+    # remove duplicates
+    linkList = f7(linkList)
+    #print(linkList)
+
+    print("Saving to file...")
+    path = 'all_players.txt'
+
+    file = codecs.open(path, "w", "utf-8")
+    file.write(str(linkList))
+    print("Saving to file done.")
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/LinkExtractor/readme.txt
@@ -0,0 +1,10 @@
+Use this tool to copy&paste all relevant links into ../crawly/start.py
+
+Usage:
+1. open command line
+2. python3.4 main.py
+3. Paste the team link
+IMPORTANT: ADD HTTP://, e.g the input has to be: www.transfermarkt.de/fc-bayern-munchen/startseite/verein/27
+NO QUOTATION MARKS
+4. copy result from all_players.txt
+5. paste into crawly/start.py
