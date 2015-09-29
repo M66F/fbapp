cab24d278d08cce4c14d5c7d5b45717b2b8a7f0f
robschu <schulz.robert.95@gmail.com>
2015-09-29 16:47:45 +0200

    passport fix

---
+++ secretConfig.js
@@ -0,0 +1,10 @@
+// Configuration settings for secret keys
+
+module.exports = {
+
+	'SESSION_SECRET' : 'keyboard cat',
+
+	'FACEBOOK_APP_ID' : 'fragt mich!',
+	'FACEBOOK_APP_SECRET' : 'xxxxxxxxxxxxxxxx',
+	'FACEBOOK_CALLBACK_URL' : 'http://localhost:3000/auth/facebook/callback'
+}
