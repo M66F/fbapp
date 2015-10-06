f5b541256f084814c3aea83aa574bda9a8681d3b
M66F <M66F@users.noreply.github.com>
2015-10-06 15:00:57 +0200

    reformatted code

--- public/js/wsclient.js
+++ public/js/wsclient.js
@@ -1,46 +1,46 @@
 var socket = io.connect();
 
 socket.on('chat', function (data) {
-    document.getElementById("content").innerHTML += '<li>' + '<small>[' + data.time + ']</small>' + '<div style="color:red;display: inline;margin-right: 40px">' + data.name + ':</div>' +  data.text + '</li>';
-    window.scrollTo(0,document.body.scrollHeight);
+    document.getElementById("content").innerHTML += '<li>' + '<small>[' + data.time + ']</small>' + '<div style="color:red;display: inline;margin-right: 40px">' + data.name + ':</div>' + data.text + '</li>';
+    window.scrollTo(0, document.body.scrollHeight);
 });
 
-socket.on('disconnect', function() {
-    document.getElementById("content").innerHTML += '<li>' + '<small>[' + new Date().toLocaleTimeString() + ']</small>' + '<div style="color:red;display: inline;margin-right: 40px">' + 'MESSAGE:' + '</div>' +  "You've been disconnected!" + '</li>';
-    window.scrollTo(0,document.body.scrollHeight);
+socket.on('disconnect', function () {
+    document.getElementById("content").innerHTML += '<li>' + '<small>[' + new Date().toLocaleTimeString() + ']</small>' + '<div style="color:red;display: inline;margin-right: 40px">' + 'MESSAGE:' + '</div>' + "You've been disconnected!" + '</li>';
+    window.scrollTo(0, document.body.scrollHeight);
 });
 
-socket.on('authorization', function(data) {
+socket.on('authorization', function (data) {
     var key = prompt("Enter chat key:");
-    socket.emit('authorization', { key: key });
+    socket.emit('authorization', {key: key});
 });
 
-socket.on('alert', function(data) {
+socket.on('alert', function (data) {
     alert(data.message);
-    if(data.message == "Authorization failed") {
-        window.location.href='/';
+    if (data.message == "Authorization failed") {
+        window.location.href = '/';
     }
 });
 
-socket.on('init', function(data) {
+socket.on('init', function (data) {
     var nameI = prompt("Enter your name:");
     document.getElementById("name").value = nameI;
     if (nameI == null) {
         document.getElementById("name").value = "ISetNoName";
         nameI = "ISetNoName";
     }
-    document.getElementById("content").innerHTML += '<li>' + '<small>[' + data.time + ']</small>' + '<div style="display: inline;margin-right: 75px">' + ':</div>' +  data.text + '</li>';
-    window.scrollTo(0,document.body.scrollHeight);
+    document.getElementById("content").innerHTML += '<li>' + '<small>[' + data.time + ']</small>' + '<div style="display: inline;margin-right: 75px">' + ':</div>' + data.text + '</li>';
+    window.scrollTo(0, document.body.scrollHeight);
     socket.emit('init', {name: nameI, text: nameI});
 });
 
-function send(){
+function send() {
     // Eingabefelder auslesen
     var name = document.getElementById("name").value;
     var text = document.getElementById("text").value;
 
-    if(text != "") {
-        if(text.charAt(0) == '/') {
+    if (text != "") {
+        if (text.charAt(0) == '/') {
             socket.emit('command', {command: text});
         } else {
             // Nachricht senden
@@ -51,9 +51,8 @@ function send(){
     var obj = document.getElementById("text").value = "";
 }
 
-
 function sendEnter() {
-    if(event.keyCode == 13) {
+    if (event.keyCode == 13) {
         send();
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,60 +2,69 @@ var express = require('express');
 var router = express.Router();
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/')
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/')
 }
 
-module.exports = function(passport){
+module.exports = function (passport) {
 //***********************************************************************************************
 //basic page routing
 
 
-router.get('/', function(req, res) {
+    router.get('/', function (req, res) {
 
-	//check if user is authenticated
-	if (req.isAuthenticated()) {
+        //check if user is authenticated
+        if (req.isAuthenticated()) {
 
-		//user is authenticated -> send his user profile
- res.render('index', {	title:"Die Fußball App",
-   						isAuthenticated : req.isAuthenticated(),
-						user : JSON.stringify({	"id" 	: req.user.id ,
-												"name"	: req.user.name.givenName,
-												"imageURL": req.user.photos[0].value})}
-			);
-  } else{
-  	// user is not authenticated
-  res.render('index', {	title:"Die Fußball App",
-   						isAuthenticated : req.isAuthenticated(),
-						user : ""}
-			);
-  };
-});
+            //user is authenticated -> send his user profile
+            res.render('index', {
+                    title: "Die Fußball App",
+                    isAuthenticated: req.isAuthenticated(),
+                    user: JSON.stringify({
+                        "id": req.user.id,
+                        "name": req.user.name.givenName,
+                        "imageURL": req.user.photos[0].value
+                    })
+                }
+            );
+        } else {
+            // user is not authenticated
+            res.render('index', {
+                    title: "Die Fußball App",
+                    isAuthenticated: req.isAuthenticated(),
+                    user: ""
+                }
+            );
+        }
+        ;
+    });
 
 //***********************************************************************************************
 //facebook routing
-router.get('/auth/facebook',
-passport.authenticate('facebook', { scope: 'public_profile' })
-);
+    router.get('/auth/facebook',
+        passport.authenticate('facebook', {scope: 'public_profile'})
+    );
 
 
 // handle the callback after facebook has authenticated the user
-	router.get('/auth/facebook/callback',
-		passport.authenticate('facebook', {
-			successRedirect : '/',
-			failureRedirect : '/'
-		})
-	);
+    router.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect: '/',
+            failureRedirect: '/'
+        })
+    );
 
 //***********************************************************************************************
-router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-router.get('/chat', function(req, res) {
-  res.render('wwsChat', {title: "Chat"});
-});
+    router.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+    router.get('/chat', function (req, res) {
+        res.render('wwsChat', {title: "Chat"});
+    });
 
 
-return router;
+    return router;
 }
