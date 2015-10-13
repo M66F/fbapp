85ad0ba367817915e5194370b58996339aca9732
PeterUlb <ulbrich-p@web.de>
2015-10-13 16:33:36 +0200

    remove chat auth

--- public/js/index.controller.js
+++ public/js/index.controller.js
@@ -518,7 +518,6 @@ if (isAuthenticated) {
 window.onkeypress = listenToTheKey;
 var parrot = 0;
 function listenToTheKey(e) {
-        // If delete key is pressed calls delete
         if (e.keyCode == 35) {
             if(parrot == 0) {
                 document.getElementById("thelogo").style.background = "url('http://i.imgur.com/9r4dE69.gif')";
