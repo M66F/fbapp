240b2d6787ce4414e1a9e675a5ae865c17743cc2
mschlechtinger <michael@schlechtinger.de>
2015-10-14 13:52:07 +0200

    Add dropdown menu for login + delete old login

--- public/js/index.controller.js
+++ public/js/index.controller.js
@@ -1,4 +1,30 @@
 //**************************************************************************************************
+//CSS
+var one = document.getElementById('dropdown'),
+    two = document.getElementById('loginButton');
+
+two.addEventListener('mouseover', function(){
+    two.style.background = "#F2F2F2";
+	two.style.boxShadow = "inset 0px 5px #51C1F1";
+	two.style.color = "#51C1F1";
+	two.style.padding = "50px 20px 34px";
+}, true);
+one.addEventListener('mouseover', function(){
+    two.style.background = "#F2F2F2";
+	two.style.boxShadow = "inset 0px 5px #51C1F1";
+	two.style.color = "#51C1F1";
+	two.style.padding = "50px 20px 34px";
+}, true);
+one.addEventListener('mouseout', function(){
+    two.style.display = "block";
+	two.style.padding = "42px 20px";
+    two.style.background = "white";
+	two.style.boxShadow = "inset 0px 0px";
+    two.style.color = "black";
+}, true);
+
+
+//**************************************************************************************************
 //Global Variables
 //"target" for the drop location of drag&drop event
 var target;
@@ -13,10 +39,7 @@ function createRightColumn() {
     if (isAuthenticated) {
         document.getElementById("login").innerHTML = "<p>Willkommen, " + user.name + " ! :)</p>" +
             "<br><img id='userImage' src=" + user.imageURL + " draggable='true' ondragstart='drag(event)'><a href='/logout' class='socialButton' >Logout</a>";
-    } else {
-        document.getElementById("login").innerHTML = "<p>Login:</p> <br>" +
-            "<a href='/auth/facebook'><img src='/static/img/LoginWithFacebookButton.jpg' /></a><a href='/auth/twitter'><img src='/static/img/Sign-in-with-Twitter.png' /></a>";
-    }  
+    }
 }
 //**************************************************************************************************
 //Speech recognition for search input field
