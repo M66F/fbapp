ab78ed93ca6c354d0c77a705d3d1c2d7cd1a7eab
robschu <schulz.robert.95@gmail.com>
2015-10-19 12:08:59 +0200

    fix vertical resize for twitter feed

--- public/js/verticalResize.js
+++ public/js/verticalResize.js
@@ -4,8 +4,8 @@ window.onresize = function() { verticalResize();};
 function verticalResize () {
 //make the container elements responsive to vertical sizing
 document.getElementById("wrapper").style.height = (window.innerHeight-100)+"px";
-document.getElementById("playerColumn").style.height = (window.innerHeight-169)+"px";
-document.getElementById("content").style.height = (window.innerHeight-391)+"px";
-document.getElementById("playerDetails").style.height = (window.innerHeight-20)+"px";
+document.getElementById("playerColumn").style.height = (window.innerHeight-149)+"px";
+document.getElementById("content").style.height = (window.innerHeight-371)+"px";
+document.getElementById("playerDetails").style.height = (window.innerHeight-100)+"px";
 
 }
