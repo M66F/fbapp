52f2de1553e4d8375251d88620708ea03d5b59cb
robschu <schulz.robert.95@gmail.com>
2015-10-19 11:57:40 +0200

    make input fields responsive

--- public/js/verticalResize.js
+++ public/js/verticalResize.js
@@ -2,8 +2,8 @@
 window.onresize = function() { verticalResize();};
 
 function verticalResize () {
-	
+//make the container elements responsive to vertical sizing
 document.getElementById("wrapper").style.height = (window.innerHeight-100)+"px";
-document.getElementById("playerColumn").style.height = (window.innerHeight-125)+"px";
-document.getElementById("content").style.height = (window.innerHeight-348)+"px";
+document.getElementById("playerColumn").style.height = (window.innerHeight-149)+"px";
+document.getElementById("content").style.height = (window.innerHeight-371)+"px";
 }
