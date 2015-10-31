b2b17f8590556a8a7192f50127c9f4abe94d7b13
mschlechtinger <michael@schlechtinger.de>
2015-11-01 00:34:47 +0100

    twitterfeed done

--- public/js/parseTwitterFeed.js
+++ public/js/parseTwitterFeed.js
@@ -1,13 +1,25 @@
 function parseTwitterFeed(twitterFeed) {
     var feed = twitterFeed.statuses;
     var returnString = "";
-    console.log(feed);
-    for(var i = 0; i < feed.length; i++) {
-        //console.log(feed[i].text);
-            returnString += feed[i].text + '<br>';
+    
+    document.getElementById('twitterFeed').behavior = "scroll";
+    document.getElementById('twitterFeed').direction = "left";
+    document.getElementById('twitterFeed').stop();
+    document.getElementById('twitterFeed').innerHTML = '<font size="2" display="block"><b>' + feed[0].text + '</b></font>';
+
+    var i = 0; //  set counter to 0
+
+    function myLoop() { //  create a loop function
+        setTimeout(function () { //  call a 10s setTimeout when the loop is called
+            document.getElementById('twitterFeed').innerHTML = '<font size="2" display="block"><b>' + feed[i].text + '</b></font>';
+
+            i++; //  increment counter
+            if (i < feed.length) { //  if the counter < feed.length, call the loop function
+                myLoop();
+            }
+        }, 10000)
     }
 
-
-    document.getElementById('twitterFeed').innerHTML = '<font size="3" display="block"><b>' + returnString + '</b></font>';
+    myLoop(); //  start the loop
 
 }
